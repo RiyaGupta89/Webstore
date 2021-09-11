39a467747d19451e6931e5061d431e5fde26c8e4
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-09-11 15:01:42 +0530

    single product page done

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ function menuToggle() {
         menuItems.style.maxHeight = "0px";
     }
 }
+
+// images
+
+let productImg = document.getElementById("product-img");
+let smallImg = document.getElementsByClassName("small-img");
+console.log(productImg);
+console.log(smallImg);
+
+smallImg[0].onclick = function () {
+    productImg.src= smallImg[0].src;
+}
+smallImg[1].onclick = function () {
+    productImg.src= smallImg[1].src;
+}
+smallImg[2].onclick = function () {
+    productImg.src= smallImg[2].src;
+}
+smallImg[3].onclick = function () {
+    productImg.src= smallImg[3].src;
+}
